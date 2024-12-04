d69b8972bd8008db0c8f705e991746758d5e900a
Kushal-Saxena999 <saxenakushal924@gmail.com>
2024-12-04 09:35:27 +0530

    Adding Lectures to Advanced Javascript

---
+++ Lecture-1/explore.js
@@ -0,0 +1,19 @@
+console.log("Kushal Saxena");
+
+function sayHello() {
+  console.log("I would like to say Hello");
+}
+
+setTimeout(() => {
+  sayHello();
+}, 4000);
+
+
+console.log("sama");
+
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+console.log("saudii");
